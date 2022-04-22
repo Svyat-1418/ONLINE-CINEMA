743edcf643e1b522dd9c84e36271d9a3019c57de
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-04-22 11:13:41 +0300

    Setting up the environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,8 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['prettier'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -16,9 +15,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 1,
+    'prettier/prettier': 0
   },
 };
